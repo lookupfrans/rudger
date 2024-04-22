36cf282b8af22738f2092e8c7eb8e07d9bb2843d
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2024-04-22 10:50:47 +0700

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC&amp;display=swap" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
+    <link rel="icon" type="image/png" sizes="180x180" href="favo_one.png">
     <style>
         h2 {
             font-size: 30px;
